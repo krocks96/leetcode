faa091237412132a5439f89775ea66c8712ea5a4
krocks96 <norinorikuronori@gmail.com>
2024-01-08 22:43:15 +0900

    improve 167

--- 167_two_sum_2/src/main.rs
+++ 167_two_sum_2/src/main.rs
@@ -1,22 +1,35 @@
 struct Solution;
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map: HashMap<i32, i32> = HashMap::new();
-        let mut result: Vec<i32> = Vec::new();
-        for i in 0..numbers.len(){
-            let diff = target - numbers[i];
-            match map.get(&diff) {
-                Some(&value) => {
-                    result.push(value);
-                    result.push((i+1) as i32);
-                    return result;
-                },
-                None => { map.insert(numbers[i], (i+1) as i32); },
+        // let mut map: HashMap<i32, i32> = HashMap::new();
+        // let mut result: Vec<i32> = Vec::new();
+        // for i in 0..numbers.len(){
+        //     let diff = target - numbers[i];
+        //     match map.get(&diff) {
+        //         Some(&value) => {
+        //             result.push(value);
+        //             result.push((i+1) as i32);
+        //             return result;
+        //         },
+        //         None => { map.insert(numbers[i], (i+1) as i32); },
+        //     }
+        // }
+        // result
+        // 2ポインタ法を使用する場合
+        let (mut left, mut right) = (0, numbers.len()-1);
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+            if sum == target {
+                return vec![left as i32 + 1, right as i32 + 1];
+            } else if sum < target {
+                left += 1;
+            } else {
+                right -= 1;
             }
         }
-        result
+        vec![]
     }
 }
 
